92e5eb3ae635980ae8bcfece6a07dcce68a53644
JaimeGonzalez <valdoc7@gmail.com>
2023-02-13 23:39:33 -0800

    String and some  methods examples

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,10 @@
+const producto = 'Monitor de 20 pulgadas';
+
+console.log(producto);
+//Conocer la longitud de la cadena de texto
+console.log(producto.length)
+
+// console.log(producto.indexOf('Monitor'));
+
+console.log(producto.includes('Tablet'));
+console.log(producto.includes('Monitor'));
