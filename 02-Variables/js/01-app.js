a34679b78ff237b4720f9087dca0a5ad38b14469
JaimeGonzalez <valdoc7@gmail.com>
2023-02-13 22:48:55 -0800

    Add variables code

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,21 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+//Las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+//JS es un lenguaje de tipo dinámico, no se especifica el tipo de dato
+producto = 20;
+console.log(producto);
+//Se pueden inicializar sin valor y asignar después
+var disponible;
+disponible = true;
+disponible = false;
+
+//Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca famosa",
+    calificacion = 5;
+
+
+
---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,8 @@
+let producto = "Tablet";
+producto = "Monitor";
+producto = 20;
+producto = true;
+producto = null;
+
+let precio;
+precio = 300;
