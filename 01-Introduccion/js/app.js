8ea375d6ed7aca2bf8a0dc789d6af2dd1e7ab7ae
JaimeGonzalez <valdoc7@gmail.com>
2023-02-13 22:24:17 -0800

    JavaScript introduction - Eslint

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,4 @@
+//Pregunta al usuario su nombre
+const nombre = prompt('Cuál es tu nombre?');
+//Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript `;
