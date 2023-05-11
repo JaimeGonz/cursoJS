1aa0fcabc0d2bd056b8ed3c34a1dc77871b6cf5b
JaimeGonzalez <valdoc7@gmail.com>
2023-05-11 17:01:29 -0600

    Add date code and format using Moment.js library

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,17 @@
+const diaHoy = new Date();
+
+let valor;
+valor = diaHoy;
+
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getHours();
+valor = diaHoy.getTime(); // Milisegundos desde el 1 de Enero de 1970
+
+
+
+
+
+
+console.log(diaHoy);
